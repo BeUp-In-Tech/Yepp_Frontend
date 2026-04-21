d3307e7b896e800e4ba30540dc417cce2eb2e54c
ahasan2912 <habib15-4284@diu.edu.bd>
2026-04-21 16:43:44 +0600

    some polishing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,18 +3,18 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-    apiKey: "AIzaSyA6_inEReD0lKuzY2pSld4-sfPHK136tOs",
-    authDomain: "yepp-app.firebaseapp.com",
-    projectId: "yepp-app",
-    storageBucket: "yepp-app.firebasestorage.app",
-    messagingSenderId: "191216477482",
-    appId: "1:191216477482:web:20c68fdc7fc557d4c2ea53"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 });
 
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
     self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
-        icon: "/logo.svg",
+        icon: "./logo.png",
     });
 });
